ac2e637922fd6c7bcf5bc3f4fd8f6c04e583b6cb
Damola Adewunmi <damolasd@gmail.com>
2019-10-03 13:33:35 +0100

    Refactored player class

--- src/player.py
+++ src/player.py
@@ -4,13 +4,15 @@
 from room import Room
 
 class Player(Room):
-    def __init__(self, name, room_name, description):
+    def __init__(self, name, room):
         self.name = name
-        super().__init__(room_name, description)
+        self.room = room
 
     def __str__(self):
-        return f"Player Name: {self.name}\t" + super().__str__() 
+        return f"Player Name: {self.name}\t" + "Room Name: " + self.room.room_name + "\tDescription: " + self.room.description
 
-player = Player("Damola", "Foyer", "Dim light")
+
+room = Room("Outside", "Outside the Cave")
+player = Player("Damola", room)
 
 print(player)
